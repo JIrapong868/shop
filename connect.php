587d22d796f631aa8cd59d5fc790b62db55a0d45
Jirapong Kongped <jirapongkongpet868@gmail.com>
2024-06-25 11:37:33 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pw = "";
+$db = "shop";
+
+// Create connection
+$conn = new mysqli($host, $user, $pw, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+mysqli_set_charset($conn, "utf8");
+?>
